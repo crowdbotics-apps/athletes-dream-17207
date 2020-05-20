806a0bf8bdf19a288abda262a1f888911c4f9303
Crowdbotics <team@crowdbotics.com>
2020-05-20 21:38:11 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/athletes_dream_17207/MainActivity.java
@@ -0,0 +1,15 @@
+package com.athletes_dream_17207;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "athletes_dream_17207";
+    }
+}
---
+++ backend/athletes_dream_17207/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for athletes_dream_17207 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'athletes_dream_17207.settings')
+
+application = get_wsgi_application()
